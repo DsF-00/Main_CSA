86059a3863ea0d0e3c22f11222e49c8eb83a92e8
DsF-00 <davidaguilar1006@gmail.com>
2025-07-16 14:09:22 -0400

    Add files via upload

---
+++ Initials_Project_DSFA_07_15_26.java
@@ -0,0 +1,13 @@
+/*David S Flores Aguilar 07/15/26 Initials project*/
+class Main{
+    public static void main(String[] args){
+    System.out.println("DSFAD        SFADSFA     DSFADSFAD         SFA  ");  //Wrote my initials in order throughout a line of code that make part of the letters
+    System.out.println("DS   FA     DS     FA    DS               FA DS");   //I used System.out.println so that every single time a line is printed, a new one is started without using /n at the end of the string
+    System.out.println("DS    FA     DSF         AD              SF   AD");   //Every single line starts out with my first initial and follows DSFA sequentially even if there is spaces in between, if theres an A in a line, the next must be DS
+    System.out.println("DS     FA       DSF      ADSFADSFA      DSFADSFAD");  //The way one line ended does not affect the next, they all start with D and follow the rest of my initials.
+    System.out.println("DS    FA          DSF    AD            SF       AD");
+    System.out.println("DS   FA     DS      FA   DS           FA         DS ");
+    System.out.println("DSFAD        SFADSFA     DS          FA           DS");
+    }
+}
+
